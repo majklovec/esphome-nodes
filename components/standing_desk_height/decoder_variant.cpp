3cf5d0cdbeb4543b4f3b6cbbf30f6bcdca88f9da
Michal Vondracek <info@michalvondracek.cz>
2024-01-16 17:06:53 +0100

    add component for table

---
+++ components/standing_desk_height/decoder_variant.cpp
@@ -0,0 +1,26 @@
+#include "decoder_variant.h"
+
+namespace esphome
+{
+  namespace standing_desk_height
+  {
+
+    const LogString *decoder_variant_to_string(DecoderVariant variant)
+    {
+      switch (variant)
+      {
+      case DECODER_VARIANT_JARVIS:
+        return LOG_STR("jarvis");
+      case DECODER_VARIANT_UPLIFT:
+        return LOG_STR("uplift");
+      case DECODER_VARIANT_OMNIDESK:
+        return LOG_STR("omnidesk");
+      case DECODER_VARIANT_LOGICDATA:
+        return LOG_STR("logicdata");
+      default:
+        return LOG_STR("unknown");
+      }
+    }
+
+  }
+}
---
+++ components/standing_desk_height/standing_desk_height.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/components/sensor/sensor.h"
+#include "esphome/components/uart/uart.h"
+
+#include "decoder.h"
+#include "jarvis_decoder.h"
+#include "uplift_decoder.h"
+#include "omnidesk_decoder.h"
+#include "logicdata_decoder.h"
+
+#include "decoder_variant.h"
+
+namespace esphome
+{
+  namespace standing_desk_height
+  {
+
+    class StandingDeskHeightSensor : public PollingComponent, public uart::UARTDevice, public sensor::Sensor
+    {
+    public:
+      void set_decoder_variant(DecoderVariant decoder_variant);
+      void start_decoder_detection();
+
+      void setup() override;
+      void loop() override;
+      void update() override;
+      void dump_config() override;
+      float get_last_read();
+
+    protected:
+      Decoder *decoder;
+      DecoderVariant decoder_variant = DECODER_VARIANT_UNKNOWN;
+
+      float last_read = -1;
+      float last_published = -1;
+
+      bool is_detecting = false;
+      uint32_t started_detecting_at = 0;
+
+      void try_next_decoder();
+    };
+
+  }
+}
